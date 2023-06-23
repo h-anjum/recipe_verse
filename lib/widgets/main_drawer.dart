7d6c2ce37e7c708ac95f9f0ec00637f4d2fc217e
Husnain Anjum <hussnainanjum06@gmail.com>
2023-06-24 03:36:16 +0500

    Add Favorite screen,Navigation Bar and new features

---
+++ lib/widgets/main_drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTie(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 26,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      onTap: () {
+        // ...
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            color: Colors.amber,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'Cooking Up!',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+                color: Theme.of(context).primaryColor,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildListTie('Meals', Icons.restaurant),
+          buildListTie('Filters', Icons.settings),
+        ],
+      ),
+    );
+  }
+}
